40d2d6f7c6e9bfed112144c78652ea6dfae857bd
TOSHIO <solution92001@gmail.com>
2025-08-26 22:17:29 +0900

    oYoリポジトリを追加

---
+++ config/40_brand/oYo/hooks/post-install.d/20-apt-oYoApplication.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# oYoオリジナルのアプリケーションをインストール
+#
+# 【目的】:
+#   - oYoリポジトリの設定を行い、oYoの独自アプリケーションをインストールする
+#
+set -e
+
+# 1) oYoリポジトリ接続用のキーリングをインストール
+dpkg -i /usr/share/openyellowos/oyo-archive-keyring_1.0_all.deb
+
+# 2) リポ登録（amd64 限定）
+install -d -m 0755 /etc/apt/keyrings
+tee /etc/apt/sources.list.d/oyo.list >/dev/null <<'EOF'
+deb [arch=amd64 signed-by=/etc/apt/keyrings/oyo-archive.gpg] https://deb.openyellowos.com kerria main
+EOF
+
+# 3) 更新
+apt update
+
+# 4) アプリケーションのインストール
+
+
